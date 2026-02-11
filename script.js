752ccc61c75b54fbdcb32f7e383ded662bdde182
neosteplabs <neosteplabs@gmail.com>
2026-02-10 22:38:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// Check if user has already confirmed
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("ageVerified") === "true") {
+    document.getElementById("age-overlay").style.display = "none";
+  }
+});
+
 function enterSite() {
+  localStorage.setItem("ageVerified", "true");
   document.getElementById("age-overlay").style.display = "none";
 }
 
