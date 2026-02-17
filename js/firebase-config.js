560674c905d9eec665cbb6a7c43f3c1976db1a92
neosteplabs <neosteplabs@gmail.com>
2026-02-16 23:36:00 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,18 @@
-https://console.firebase.google.com
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOfAR0avke6T9OJGusMhUO9OGXpvjd7pI",
+  authDomain: "neostep-portal-b9ea3.firebaseapp.com",
+  projectId: "neostep-portal-b9ea3",
+  storageBucket: "neostep-portal-b9ea3.appspot.com",
+  messagingSenderId: "312972875460",
+  appId: "1:312972875460:web:20cb8d4663b436f9a09b91"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
